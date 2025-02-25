e182e2ff07dfbae3e84195bea978a519b2264e13
alecnissen <alecnissen@yahoo.com>
2025-02-25 16:49:41 -0500

    username frontend form validation all ok, moving onto frontend password form validation

--- frontend/src/Create_User.jsx
+++ frontend/src/Create_User.jsx
@@ -1,60 +1,132 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-
-
-
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Create_User() {
-
-const handleChange = (e) => { 
-  const { name, value } = e.target;
-  setCreateUserFormData({
-    ...createUserFormData,
-    [name]: value
-  });
-} 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCreateUserFormData({
+      ...createUserFormData,
+      [name]: value,
+    });
+  };
 
   let [createUserFormData, setCreateUserFormData] = useState({
-    username: '',
-    password: ''
-  })
+    username: "",
+    password: "",
+  });
+
+  let [formErrors, setFormErrors] = useState({});
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (!data.username.trim()) {
+      errors.username = "Username is required";
+    } else if (data.username.length < 5) {
+      errors.username = "Username must be at least 5 characters";
+    }
+
+    if (!data.password) {
+      errors.password = "Password is required";
+    } else if (data.password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+
+    if (data.confirmPassword !== data.password) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    return errors;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   
-    axios.post("http://localhost:3000/create_user_form/create_user", createUserFormData)
 
-    .then((response) => {
-      console.log(response.status, response.data);
-    })
+    const newErrors = validateForm(createUserFormData);
+    setFormErrors(newErrors);
+
+    axios
+      .post(
+        "http://localhost:3000/create_user_form/create_user",
+        createUserFormData
+      )
+
+      .then((response) => {
+        console.log(response.status, response.data);
+
+       
+
+        if (Object.keys(newErrors).length === 0) {
+          console.log("form submitted successfully");
+        } else {
+          console.log("form submission failed due to errors");
+        }
+      });
 
     setCreateUserFormData({
-      username: '',
-      password: ''
-    })
-
-    .catch((error) => {
+      username: "",
+      password: "",
+    }).catch((error) => {
       console.log(error);
       alert("Submission Failed");
-    })
+    });
 
 
-  }; 
+
+
+  };
+
+  // const validateForm = (data) => {
+  //   const errors = {};
+
+  //   if (!data.username.trim()) {
+  //     errors.username = "Username is required";
+  //   } else if (data.username.length < 5) {
+  //     errors.username = "Username must be at least 5 characters";
+  //   }
+
+  //   if (!data.password) {
+  //     errors.password = "Password is required";
+  //   } else if (data.password.length < 8) {
+  //     errors.password = "Password must be at least 8 characters long";
+  //   }
+
+  //   if (data.confirmPassword !== data.password) {
+  //     errors.confirmPassword = "Passwords do not match";
+  //   }
+
+  //   return errors;
+  // };
 
   return (
-   <> 
-   <form method='post' onSubmit={handleSubmit}>
-    <label>Username</label>
-    <input type="text" placeholder="username" name='username' value={createUserFormData.username} onChange={handleChange}></input>
+    <>
+      <form method="post" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          value={createUserFormData.username}
+          onChange={handleChange}
+          required
+        ></input>
 
-    <label>Password</label>
-    <input type="text" placeholder="password" name='password' value={createUserFormData.password} onChange={handleChange}></input>
+        {formErrors.username && (
+          <span className="error-message">{formErrors.username}</span>
+        )}
 
-    <button type='submit'>Create User</button>
-   </form>
+        <label>Password</label>
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={createUserFormData.password}
+          onChange={handleChange}
+          required
+        ></input>
 
-   </>
-  )
+        <button type="submit">Create User</button>
+      </form>
+    </>
+  );
 }
-
-
